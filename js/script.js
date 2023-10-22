8dcd7879820b0bececd96059c8fbae1233d12694
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-10-22 10:24:57 +0300

    Updated functionality to handle changes to the base currency selected including updating conversion rates, added comments for the different sections

--- js/script.js
+++ js/script.js
@@ -2,20 +2,41 @@ baseCurrencyCodeSelector = document.getElementById('base-currencies')
 targetCurrencyCodeSelector = document.getElementById('target-currencies')
 currencyAmountInput = document.getElementById('amount')
 convertedAmountSection = document.getElementById('converted')
+conversion_rates = {}
 
+/**
+ * Handles defaults displayed during initial page load
+ */
 document.addEventListener('DOMContentLoaded', (e) => {
     updateConvertedCurrency()
 }, true)
 
-
-function updateConvertedCurrency() {
+/**
+ * Handle update to base currency selected. Updates conversion rates by
+ * requesting conversion rates matching the base currency selected.
+ * Handled separately to reduce unnecessary requests that may be made 
+ * when changing target currency or the amount to be converted instead.
+ */
+function handleBaseCurrencyUpdate() {
     baseCode = document.getElementById('base-currencies').value
+
+    fetch('http://127.0.0.1:5555/')
+        .then(res => res.json())
+        .then(data => {
+            conversion_rates = data['conversion_rates']
+        })
+    
+    // Update displayed amount to match updated rates for the base currency
+    updateConvertedCurrency()
+}
+
+/**
+ * Handle any updates to target currency and amount to be converted. Handles 
+ * display of the converted amount.
+ */
+function updateConvertedCurrency() {
     targetCode = document.getElementById('target-currencies').value
     amount = currencyAmountInput = document.getElementById('amount').value == '' ? 0 : currencyAmountInput = document.getElementById('amount').value
-
-    // fetch('http://127.0.0.1:5555/')
-    //     .then(res => res.json())
-    //     .then(data => console.log(data))
 
     convertedAmountSection.innerText = new Intl.NumberFormat("en-US", {
         style: "currency",
@@ -25,6 +46,6 @@ function updateConvertedCurrency() {
     }).format(Number(amount));
 }
 
-baseCurrencyCodeSelector.addEventListener('input', updateConvertedCurrency)
+baseCurrencyCodeSelector.addEventListener('input', handleBaseCurrencyUpdate)
 targetCurrencyCodeSelector.addEventListener('input', updateConvertedCurrency)
 currencyAmountInput.addEventListener('input', updateConvertedCurrency)
